6f4b66b5e89e870e631103cb795571d6348890c2
‘orihers6’ <orihers6@gmail.com>
2018-08-12 16:15:02 +0300

    select horse add empty field takenDays for horses

--- ApplicationServer/www/matches_wizard.js
+++ ApplicationServer/www/matches_wizard.js
@@ -24,14 +24,16 @@ function createNewRiderElement(id, firstName, lastName, age, classTime) {
     let inputValue = firstName + " " + lastName + "   Age:" + age + "   Class Time:" + classTime;
     let t = document.createTextNode(inputValue);
     li.appendChild(t);
-    li.setAttribute("id", id);
+    li.setAttribute("riderId", id);
     li.setAttribute("dateForInstructor", classTime);
-    li.setAttribute("onclick", "selectRider(this.getAttribute('id'), this.getAttribute('dateForInstructor'))");
+    li.setAttribute("onclick", "selectRider(this.getAttribute('riderId'), this.getAttribute('dateForInstructor'))");
     document.getElementById("myULRiders").appendChild(li);
 }
 
-function selectRider(idRider, date)
+function selectRider(riderId, date)
 {
+    document.getElementById("myULInstructors").innerHTML="";
+    document.getElementById("myULHorses").innerHTML="";
     let dateFormat = new Date(date);
     let day = dateFormat.getDay();
     fetch("http://localhost:3000/instructors/" + day, {
@@ -44,26 +46,28 @@ function selectRider(idRider, date)
     }).then((response) => response.json())
         .then((instructors) => {
             instructors.forEach(function (instructor) {
-                createNewInstructorElement(instructor.guid, instructor.firstName, instructor.lastName,instructor.email, date)
+                createNewInstructorElement(instructor.guid, instructor.firstName, instructor.lastName,instructor.email, date, riderId)
             });
         });
 
 }
 
-function createNewInstructorElement(id, firstName, lastName, email, date) {
+function createNewInstructorElement(id, firstName, lastName, email, date, riderId) {
     let li = document.createElement("li");
     let inputValue = firstName + " " + lastName + "   Email:" + email;
     let t = document.createTextNode(inputValue);
     li.appendChild(t);
-    li.setAttribute("id", id);
+    li.setAttribute("instructorID", id);
+    li.setAttribute("riderId", riderId);
     li.setAttribute("date", date);
-    li.setAttribute("onclick","selectInstructor(this.getAttribute('id'),this.getAttribute('date'))");
+    li.setAttribute("onclick","selectInstructor(this.getAttribute('instructorID'),this.getAttribute('date'),this.getAttribute('riderId'))");
     document.getElementById("myULInstructors").appendChild(li);
 }
 
 
-function selectInstructor(idInstructor, date)
+function selectInstructor(instructorId, date, riderId)
 {
+    document.getElementById("myULHorses").innerHTML="";
     let dateFormat = new Date(date);
     fetch("http://localhost:3000/horses/" + dateFormat, {
         method: 'GET',
@@ -75,16 +79,18 @@ function selectInstructor(idInstructor, date)
     }).then((response) => response.json())
         .then((horses) => {
             horses.forEach(function (horse) {
-                createNewHorseElement(horse.guid, horse.name, horse.breed,horse.picture, date)
+                createNewHorseElement(horse.guid, horse.name, horse.breed,horse.picture, date, riderId, instructorId)
             });
         });
 
 }
 
-function createNewHorseElement(id, name, breed, picture, date) {
+function createNewHorseElement(id, name, breed, picture, date, riderId, instructorId) {
     let li = document.createElement("li");
     li.innerHTML = `<h5>${name}</h5> <img src="https://zippy.gfycat.com/UnconsciousAngelicAmericangoldfinch.gif">`;
-    li.setAttribute("id", id);
+    li.setAttribute("horseId", id);
+    li.setAttribute("riderId", riderId);
+    li.setAttribute("instructorId", instructorId);
     li.setAttribute("date", date);
     // li.setAttribute("onclick",selectInstructor(this.getAttribute('id'),this.getAttribute('date')));
     document.getElementById("myULHorses").appendChild(li);
