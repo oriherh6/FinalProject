9c1f95f042f01e4a36d33fd1d09da134f2251e38
‘orihers6’ <orihers6@gmail.com>
2018-08-12 12:57:52 +0300

    change days to numbers select rider and get instructors

--- ApplicationServer/www/matches_wizard.js
+++ ApplicationServer/www/matches_wizard.js
@@ -12,18 +12,51 @@ function loadRiders() {
     }).then((response) => response.json())
         .then((riders) => {
             riders.forEach(function (rider) {
-                createNewTodoElement(rider.guid, rider.firstName, rider.lastName,rider.age, rider.classTime)
+                createNewRiderElement(rider.guid, rider.firstName, rider.lastName,rider.age, rider.classTime)
             });
         });
 }
 
 
 
-function createNewTodoElement(id, firstName, lastName, age, classTime) {
+function createNewRiderElement(id, firstName, lastName, age, classTime) {
     let li = document.createElement("li");
     let inputValue = firstName + " " + lastName + "   Age:" + age + "   Class Time:" + classTime;
     let t = document.createTextNode(inputValue);
     li.appendChild(t);
     li.setAttribute("id", id);
-    document.getElementById("myUL").appendChild(li);
+    li.setAttribute("dateForInstructor", classTime);
+    li.setAttribute("onclick", "selectRider(this.getAttribute('id'), this.getAttribute('dateForInstructor'))");
+    document.getElementById("myULRiders").appendChild(li);
+}
+
+function selectRider(idRider, date)
+{
+    let dateFormat = new Date(date);
+    let day = dateFormat.getDay();
+    fetch("http://localhost:3000/instructors/" + day, {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }).then((response) => response.json())
+        .then((instructors) => {
+            instructors.forEach(function (instructor) {
+                createNewRiderElement(instructor.guid, instructor.firstName, instructor.lastName,instructor.email, instructor.classTime)
+            });
+        });
+
+}
+
+function createNewInstructorElement(id, firstName, lastName, email, day) {
+    let li = document.createElement("li");
+    let inputValue = firstName + " " + lastName + "   Email:" + email + "   Day:" + day;
+    let t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    li.setAttribute("id", id);
+    // li.setAttribute("date", date);
+    // li.setAttribute("onclick",selectRider(this.getAttribute('id'),this.getAttribute('date')));
+    document.getElementById("myULInstructors").appendChild(li);
 }
